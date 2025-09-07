d8d257ce91ff692fe802b070d0ac8d7e9e7c8f65
Kirill2402100 <miroshkin@inbox.ru>
2025-09-07 18:02:48 +0200

    Update scanner_bmr_dca.py

--- scanner_bmr_dca.py
+++ scanner_bmr_dca.py
@@ -11,7 +11,8 @@ import gspread
 
 # ИЗМЕНЕНО: импорты для Forex
 from fx_mt5_adapter import FX, margin_to_lots, lots_to_margin, default_tick
-from fx_feed import fetch_fx_ohlcv as fetch_ohlcv_yf  # <-- ВАЖНО: правильная функция
+# ДОЛЖНО БЫТЬ
+from fx_feed import fetch_ohlcv as fetch_ohlcv_yf
 
 import trade_executor
 
